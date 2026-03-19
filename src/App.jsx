c7b464245b139456a54d7112fa7798d39f087b74
Gift07 <giftedwards9@gmail.com>
2026-03-19 12:58:03 +0300

    added redirect logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function App() {
   const handleDownload = () => {
     window.location.href =
       "https://expo.dev/artifacts/eas/hWrHJ7ADhzaynEvTi6dbnd.apk";
   };
+
+  const redirectUrl = "https://winzone-web.vercel.app/";
+
+  useEffect(() => {
+    const isInstagram = /Instagram/i.test(navigator.userAgent);
+
+    if (isInstagram) {
+      const clean = redirectUrl.replace("https://", "");
+
+      window.location.href =
+        "intent://" +
+        clean +
+        "#Intent;scheme=https;package=com.android.chrome;end;";
+
+      setTimeout(() => {
+        window.location.href = redirectUrl;
+      }, 1500);
+    }
+  }, []);
 
   return (
     <div style={styles.container}>
